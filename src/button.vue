4be8668e1709823ee6d66c39e674900eefc88b39
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2019-04-15 18:14:01 +0800

    copy

---
+++ src/button.vue
@@ -0,0 +1,51 @@
+<template>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+
+<style lang="scss">
+    @keyframes spin { 0%{transform: rotate(0deg)} 100%{transform: rotate(360deg)} }
+    .g-button{
+        font-size:var(--font-size);height: var(--button-height);padding: 0 1.1em;border-radius:var(--border-radius);border:1px solid var(--border-color);background:var(--button-bg);display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
+        &:hover{border-color:var(--border-color-hover);}
+        &:active{background-color:var(--button-active-bg) ;}
+        &:focus{outline: none;}
+        >.icon{order: 1;margin-right: .3em;margin-left: 0;}
+        >.content{order: 2;}
+        &.icon-right{
+            >.icon{order: 2;margin-left: .3em;margin-right: 0;}
+            >.content{order: 1;}
+        }
+        .loading{animation: spin 2s infinite linear;}
+    }
+</style>
+
+<script>
+    import Icon from './icon'
+    export default {
+        components:{
+            'g-icon':Icon
+        },
+        props:{
+            icon:{},
+            loading:{
+                type: Boolean,
+                default:false
+            },
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return value === 'left' || value === 'right';
+                }
+            }
+        },
+
+    }
+</script>
