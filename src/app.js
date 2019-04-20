cb0165d43c929cc3cb1520177e68303fc5384063
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2019-04-20 10:01:40 +0800

    row 和 col 的雏形完成

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from "./icon"
 import ButtonGtoup from './button-group'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGtoup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 
 new Vue({
     el:"#app",
