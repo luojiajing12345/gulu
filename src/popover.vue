167816a41f062f5f09d993ba07f87c58ed2722a1
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2019-04-25 20:29:16 +0800

    解决抖动bug

--- src/popover.vue
+++ src/popover.vue
@@ -166,11 +166,13 @@
 
             &::before {
                 border-top-color: black;
+                border-bottom: none;
                 top: 100%;
             }
 
             &::after {
                 border-top-color: white;
+                border-bottom: none;
                 top: calc(100% - 1px);
             }
         }
@@ -184,11 +186,13 @@
 
             &::before {
                 border-bottom-color: black;
+                border-top: none;
                 bottom: 100%;
             }
 
             &::after {
                 border-bottom-color: white;
+                border-top: none;
                 bottom: calc(100% - 1px);
             }
         }
@@ -204,11 +208,13 @@
 
             &::before {
                 border-left-color: black;
+                border-right: none;
                 left: 100%;
             }
 
             &::after {
                 border-left-color: white;
+                border-right: none;
                 left: calc(100% - 1px);
             }
         }
@@ -223,11 +229,13 @@
 
             &::before {
                 border-right-color: black;
+                border-left: none;
                 right: 100%;
             }
 
             &::after {
                 border-right-color: white;
+                border-left: none;
                 right: calc(100% - 1px);
             }
         }
