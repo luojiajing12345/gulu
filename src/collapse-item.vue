a03e1a734dc36d7ed52ef897cf0f968da3751fca
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2019-04-26 10:20:00 +0800

    Collapse 完成

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,83 +1,68 @@
 <template>
     <div class="collapseItem">
-        <div class="title" @click="toggle">
+        <div class="title" @click="toggle" :data-name="name">
             {{title}}
         </div>
-        <div class="content" v-if="open">
+        <div class="content" ref="content" v-if="open">
             <slot></slot>
         </div>
-
     </div>
-
 </template>
 
 <script>
     export default {
-        name:"GuluCollapseItem",
-        props:{
-            title:{
-                type:String,
-                required:true
+        name: "GuluCollapseItem",
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
             }
         },
-        data(){
-            return{
-                open:false
+        data () {
+            return {
+                open: false,
             }
         },
-        inject:['eventBus'],
-        mounted() {
-            this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
-                if (vm !== this){
-                    this.close()
+        inject: ['eventBus'],
+        mounted () {
+            this.eventBus && this.eventBus.$on('update:selected', (names) => {
+                if (names.indexOf(this.name) >= 0) {
+                    this.open = true
+                } else {
+                    this.open = false
                 }
             })
         },
-        methods:{
-            toggle(){
-                if (this.open){
-                    this.open = false
-                }else{
-                    this.open = true
-                    this.eventBus && this.eventBus.$emit('update:selected',this)
+        methods: {
+            toggle () {
+                if (this.open) {
+                    this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
+                } else {
+                    this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
                 }
             },
-            close(){
-                this.open =  false
-            }
-        }
-
+        },
     }
 </script>
 
 <style scoped lang="scss">
-    $grey:#ddd;
-    $border-radius:4px;
-    .collapseItem{
-        >.title{
-            border:1px solid $grey;
-            margin-top:-1px ;
-            margin-left: -1px;
-            margin-right: -1px;
-            min-height: 32px;
-            display: flex;
-            justify-items: center;
-            padding: 0 8px;
+    $grey: #ddd;
+    $border-radius: 4px;
+    .collapseItem {
+        > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
+            min-height: 32px; display: flex; align-items: center; padding: 0 8px;
+            background: lighten($grey, 8%);
         }
-        &:first-child{
-            >.title{
-                border-top-left-radius: $border-radius;
-                border-top-right-right: $border-radius;
-            }
+        &:first-child {
+            > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
         }
-        &:last-child{
-            >.title:last-child{
-                border-bottom-left-radius: $border-radius;
-                border-bottom-right-right: $border-radius;
-            }
+        &:last-child {
+            > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
         }
-        >.content{
-            padding: 8px;
-        }
+        > .content { padding: 8px; }
     }
 </style>
