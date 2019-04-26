e1624dc7367796929fa312d5f220b54fd1687653
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2019-04-26 18:29:20 +0800

    忘记运行deploy.sh

--- docs/.vuepress/components/toast-demo-1.vue
+++ docs/.vuepress/components/toast-demo-1.vue
@@ -10,7 +10,6 @@
             <g-button @click="$toast('点击弹出提示', {position:'middle'})">中间弹出</g-button>
             <g-button @click="$toast('点击弹出提示', {position:'bottom'})">下方弹出</g-button>
         </div>
-
         <p>
             <strong>代码</strong>
         </p>
